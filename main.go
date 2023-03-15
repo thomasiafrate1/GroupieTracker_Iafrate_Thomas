fdb0928774971da8849aa84db3561651d30fc5a6
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2023-03-15 17:32:05 +0100

    CartePageAccueil

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
-	"strconv"
+	"strings"
 )
 
 type Character struct {
@@ -20,28 +21,85 @@ type Character struct {
 	ID              int      `json:"_id"`
 	ImageURL        string   `json:"imageUrl"`
 	URL             string   `json:"url"`
+	Choix           int
 }
 
-func getCharacter(id int) (*Character, error) {
+type AllCharacter struct {
+	Data []struct {
+		Films           []any  `json:"films"`
+		ShortFilms      []any  `json:"shortFilms"`
+		TvShows         []any  `json:"tvShows"`
+		VideoGames      []any  `json:"videoGames"`
+		ParkAttractions []any  `json:"parkAttractions"`
+		Allies          []any  `json:"allies"`
+		Enemies         []any  `json:"enemies"`
+		ID              int    `json:"_id"`
+		Name            string `json:"name"`
+		ImageURL        string `json:"imageUrl"`
+		URL             string `json:"url"`
+	} `json:"data"`
+	Count      int    `json:"count"`
+	TotalPages int    `json:"totalPages"`
+	NextPage   string `json:"nextPage"`
+	Choix      int
+}
+
+func getCharacter(id int) Character {
 	url := fmt.Sprintf("https://api.disneyapi.dev/characters/%d", id)
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
 	}
 	defer resp.Body.Close()
 
-	var character Character
-	err = json.NewDecoder(resp.Body).Decode(&character)
+	data, Rerr := ioutil.ReadAll(resp.Body)
+	if Rerr != nil {
+		fmt.Println(Rerr)
+	}
+	fmt.Println(data)
+	character := Character{}
+	err = json.Unmarshal(data, &character)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
 	}
 
-	return &character, nil
+	if len(character.Films) > 2 {
+		character.Films = character.Films[:2]
+	}
+
+	return character
+}
+
+func fullCharacter(page int) AllCharacter {
+	url := fmt.Sprintf("https://api.disneyapi.dev/characters?page=%d", page)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	data, Rerr := ioutil.ReadAll(resp.Body)
+	if Rerr != nil {
+		fmt.Println(Rerr)
+	}
+
+	character := AllCharacter{}
+	err = json.Unmarshal(data, &character)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return character
 }
 
 func main() {
+	verif_id := false
+	static := http.FileServer(http.Dir("css"))
+	http.Handle("/css/", http.StripPrefix("/css/", static))
 	tmpl := template.Must(template.ParseFiles("pages/personnages.html"))
+	page := 1
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
@@ -51,46 +109,37 @@ func main() {
 				return
 			}
 
-			idStr := r.FormValue("id")
-			id, err := strconv.Atoi(idStr)
-			if err != nil {
-				http.Error(w, "Invalid ID", http.StatusBadRequest)
-				return
-			}
-
-			character, err := getCharacter(id)
-			if err != nil {
-				http.Error(w, "Character not found", http.StatusNotFound)
-				return
-			}
-
-			data := struct {
-				Name       string
-				ImageURL   string
-				Films      []string
-				VideoGames []string
-				Allies     []string
-				Enemies    []string
-				TvShows    []string
-			}{
-				Name:       character.Name,
-				ImageURL:   character.ImageURL,
-				Films:      character.Films,
-				VideoGames: character.VideoGames,
-				Allies:     character.Allies,
-				Enemies:    character.Enemies,
-				TvShows:    character.TvShows,
-			}
-
-			err = tmpl.Execute(w, data)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
+			for page = 1; !verif_id; page++ {
+				Name := r.FormValue("Name")
+				fmt.Println(Name)
+				var id int
+				test := AllCharacter{}
+				test = fullCharacter(page)
+				for i := 0; i < len(test.Data); i++ {
+					if strings.Compare(strings.ToLower(Name), strings.ToLower(test.Data[i].Name)) == 0 {
+						id = test.Data[i].ID
+						verif_id = true
+					}
+				}
+				if verif_id {
+					data := Character{}
+					data = getCharacter(id)
+					data.Choix = 2
+					err = tmpl.Execute(w, data)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					break
+				}
 			}
 		} else {
-			tmpl.Execute(w, nil)
+			test := AllCharacter{}
+			test = fullCharacter(page)
+			test.Choix = 1
+			tmpl.Execute(w, test)
 		}
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8888", nil)
 }
